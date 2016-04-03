ccbbed987c3430f3023845aa6b5e93d9038a7139
Martin Journois <martin.journois@fr.michelin-solutions.com>
2016-04-03 19:44:19 +0200

    Fix pep8 tests

--- services/main/main.py
+++ services/main/main.py
@@ -1,31 +1,36 @@
-import yaml, os
+import yaml
+import os
 from tornado import ioloop, web, httpserver, httpclient
 
 service_name = os.path.dirname(__file__).split('/')[-1]
-root_path = os.path.abspath(os.path.join(os.path.dirname(__file__),'../..'))
-config = yaml.load(open(os.path.join(root_path,'config.yml')))
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+config = yaml.load(open(os.path.join(root_path, 'config.yml')))
 
-conf = config['services'].get(service_name,None)
+conf = config['services'].get(service_name, None)
 assert conf is not None
+
 
 class AsynchronousHandler(web.RequestHandler):
     @web.asynchronous
     def get(self, service, uri=''):
         client = httpclient.AsyncHTTPClient()
-        conf = config['services'].get(service,None)
+        conf = config['services'].get(service, None)
         if conf is None:
-            raise web.HTTPError(500,reason='Service {} not known.'.format(service))
-        url = conf.get('url',None)
+            raise web.HTTPError(500, reason='Service {} not known.'.format(service))
+        url = conf.get('url', None)
         if url is None:
-            raise web.HTTPError(500,reason='Service {} has no url.'.format(service))
-        response = client.fetch(url + uri,self.on_response)
+            raise web.HTTPError(500, reason='Service {} has no url.'.format(service))
+        client.fetch(url + uri, self.on_response)
+
     def on_response(self, response):
         self.write(response.body)
         self.finish()
 
+
 class QueryHandler(web.RequestHandler):
     def get(self, url):
         self.write("Hello from main/" + url)
+
 
 app = web.Application([
     ("/", QueryHandler),
@@ -37,4 +42,3 @@ server = httpserver.HTTPServer(app)
 server.bind(conf['port'])
 server.start(conf['threads_nb'])
 ioloop.IOLoop.current().start()
-
--- services/service2/main.py
+++ services/service2/main.py
@@ -1,12 +1,14 @@
-import yaml, os
-from tornado import ioloop, web, httpserver, httpclient
+import yaml
+import os
+from tornado import ioloop, web, httpserver
 
 service_name = os.path.dirname(__file__).split('/')[-1]
-root_path = os.path.abspath(os.path.join(os.path.dirname(__file__),'../..'))
-config = yaml.load(open(os.path.join(root_path,'config.yml')))
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+config = yaml.load(open(os.path.join(root_path, 'config.yml')))
 
-conf = config['services'].get(service_name,None)
+conf = config['services'].get(service_name, None)
 assert conf is not None
+
 
 class QueryHandler(web.RequestHandler):
     def get(self, param=''):
@@ -20,4 +22,3 @@ server = httpserver.HTTPServer(app)
 server.bind(conf['port'])
 server.start(conf['threads_nb'])
 ioloop.IOLoop.current().start()
-
